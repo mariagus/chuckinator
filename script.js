400fbd6cdc78a8c399f77b9ad131911123255933
Maria Gusova <masha.gu@gmail.com>
2021-04-08 16:34:45 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+//create random chuck norris joke app
+// send a get req to https://api.chucknorris.io/jokes/random will result in a random chuck norris joke being sent to you!
+// add button that fetches brand new random joke
+let h1 = document.querySelector('h1');
+let btn = document.querySelector('#btn');
+
+
+btn.addEventListener('click', ()=>{
+    fetch('https://api.chucknorris.io/jokes/random')
+    .then(response => response.json())
+    .then(data => {
+      h1.textContent = `${data.value}`;
+    });
+})

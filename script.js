d77fbeed9846d7b2a512cf87f3473e9d4e7b0ab2
Maria Gusova <masha.gu@gmail.com>
2021-04-10 22:32:11 +0100

    change format and font

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 //create random chuck norris joke app
 // send a get req to https://api.chucknorris.io/jokes/random will result in a random chuck norris joke being sent to you!
 // add button that fetches brand new random joke
-let h1 = document.querySelector('h1');
-let btn = document.querySelector('#btn');
+let h2 = document.querySelector("h2");
+let btn = document.querySelector("#btn");
 
-
-btn.addEventListener('click', ()=>{
-    fetch('https://api.chucknorris.io/jokes/random')
-    .then(response => response.json())
-    .then(data => {
-      h1.textContent = `${data.value}`;
+btn.addEventListener("click", () => {
+  fetch("https://api.chucknorris.io/jokes/random")
+    .then((response) => response.json())
+    .then((data) => {
+      h2.textContent = `${data.value}`;
     });
-})
+});
